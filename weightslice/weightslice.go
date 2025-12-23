7d9f1cc27df16001a33116c85527969774cc5b90
David Bond <davidsbond93@gmail.com>
2025-12-23 21:42:02 +0000

    x/weightslice: use entry identifiers
    This commit modifies things under the hood for the `weightslice` package.
    
    Each element is now assigned an identifier that matches their starting
    index of when they were appended. This gives a stable identifier with
    which we can always reference.
    
    This is important as in concurrent scenarios the range is being performed
    over a snapshot of the underlying slice and indexes may change due to
    calls to sort in other goroutines. Using a fixed identifier means that
    calls to `SetWeight` will always refer to a specific element, rather
    than whatever element lives at that index.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- weightslice/weightslice.go
+++ weightslice/weightslice.go
@@ -14,13 +14,14 @@ type (
 	// the slice. The Slice is sorted on calls to Append and SetWeight.
 	Slice[T any, W cmp.Ordered] struct {
 		mutex    sync.RWMutex
-		elements []entry[T, W]
-		compare  func(a, b entry[T, W]) int
+		elements []*entry[T, W]
+		compare  func(a, b *entry[T, W]) int
 	}
 
 	entry[V any, Weight comparable] struct {
 		value  V
 		weight Weight
+		id     int
 	}
 )
 
@@ -34,18 +35,18 @@ const (
 // New returns a new Slice instance that will sort elements by weight in the specified sort order. The sort order can
 // either be Ascending or Descending. For any other value, Ascending is assumed.
 func New[T any, W cmp.Ordered](sort uint) *Slice[T, W] {
-	compare := func(a, b entry[T, W]) int {
+	compare := func(a, b *entry[T, W]) int {
 		return cmp.Compare(a.weight, b.weight)
 	}
 
 	if sort == Descending {
-		compare = func(a, b entry[T, W]) int {
+		compare = func(a, b *entry[T, W]) int {
 			return cmp.Compare(b.weight, a.weight)
 		}
 	}
 
 	return &Slice[T, W]{
-		elements: make([]entry[T, W], 0),
+		elements: make([]*entry[T, W], 0),
 		compare:  compare,
 	}
 }
@@ -55,22 +56,31 @@ func (s *Slice[T, W]) Append(value T, weight W) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	e := entry[T, W]{
+	e := &entry[T, W]{
 		value:  value,
 		weight: weight,
+		id:     len(s.elements) + 1,
 	}
 
 	s.elements = append(s.elements, e)
 	slices.SortFunc(s.elements, s.compare)
 }
 
-// SetWeight modifies the weight value of an element at a specified index. This method will panic if the index is
-// out of bounds.
-func (s *Slice[T, W]) SetWeight(idx int, weight W) {
+// SetWeight modifies the weight value of an element by its id. The provided identifier is internal and only provided
+// to the consumer as the first value when using Range.
+func (s *Slice[T, W]) SetWeight(id int, weight W) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	s.elements[idx].weight = weight
+	for _, e := range s.elements {
+		if e.id != id {
+			continue
+		}
+
+		e.weight = weight
+		break
+	}
+
 	slices.SortFunc(s.elements, s.compare)
 }
 
@@ -82,8 +92,8 @@ func (s *Slice[T, W]) Range() iter.Seq2[int, T] {
 	s.mutex.RUnlock()
 
 	return func(yield func(int, T) bool) {
-		for i, e := range snapshot {
-			if !yield(i, e.value) {
+		for _, e := range snapshot {
+			if !yield(e.id, e.value) {
 				return
 			}
 		}
