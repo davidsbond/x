d39cb40efe728694ebe048778f96d613b46d9ea2
David Bond <davidsbond93@gmail.com>
2026-02-01 15:06:01 +0000

    x/convert: add Map function
    This commit adds a new function `convert.Map`, which works similarly
    to `convert.Slice` but accepts and converts map values from one type
    to another.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- convert/convert.go
+++ convert/convert.go
@@ -2,10 +2,21 @@
 package convert
 
 // Slice maps a slice of one type into a slice of another using the provided conversion function.
-func Slice[In, Out any](in []In, fn func(In) Out) []Out {
-	out := make([]Out, len(in))
+func Slice[Have, Want any](in []Have, fn func(Have) Want) []Want {
+	out := make([]Want, len(in))
 	for i := range in {
 		out[i] = fn(in[i])
+	}
+
+	return out
+}
+
+// Map consumes a map, running each key and value through a conversion function, returning a new map with the same
+// keys but transformed values.
+func Map[Key comparable, Have any, Want any](in map[Key]Have, fn func(Key, Have) Want) map[Key]Want {
+	out := make(map[Key]Want, len(in))
+	for k, v := range in {
+		out[k] = fn(k, v)
 	}
 
 	return out
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -15,8 +15,25 @@ func TestSlice(t *testing.T) {
 	input := []int{1, 2, 3}
 	expected := []string{"1", "2", "3"}
 
-	actual := convert.Slice(input, func(in int) string {
-		return strconv.Itoa(in)
+	actual := convert.Slice(input, func(value int) string {
+		assert.Contains(t, input, value)
+
+		return strconv.Itoa(value)
+	})
+
+	assert.EqualValues(t, expected, actual)
+}
+
+func TestMap(t *testing.T) {
+	t.Parallel()
+
+	input := map[string]int{"a": 1, "b": 2, "c": 3}
+	expected := map[string]string{"a": "1", "b": "2", "c": "3"}
+
+	actual := convert.Map(input, func(key string, value int) string {
+		assert.Contains(t, input, key)
+
+		return strconv.Itoa(value)
 	})
 
 	assert.EqualValues(t, expected, actual)
